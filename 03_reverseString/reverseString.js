add23acc878ca2ddf0c2bdf819d2bf0dae7f104c
Cowpacino <araj7915@gmail.com>
2024-08-12 02:36:35 +0530

    update functions to pass tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let right = string.length - 1;
+    let result = ""
+    while(right >= 0) {
+        result += string[right];
+        right -= 1;
+    }
+    return result;
 };
 
 // Do not edit below this line
