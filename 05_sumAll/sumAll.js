cf258a2b13b2560dbe3c16e09d7bd12ed612f241
Cowpacino <araj7915@gmail.com>
2024-08-12 17:04:48 +0530

    fix function to its intended purpose

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+    if (start <= -1 || end <= -1) {
+        return `ERROR`
+    }
 
+    if (Number.isInteger(start) && Number.isInteger(end)) { 
+
+        result = 0
+        if (start > end) {
+            for (let i = start; i >= end; i--) {
+                result += i;
+            }
+            return result;
+        }
+        else {
+            for (let i = 1; i <= end; i++) {
+                result += i
+            }
+            return result;
+        }
+    }
+    return `ERROR`
 };
 
 // Do not edit below this line
